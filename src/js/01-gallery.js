ceed3c1a46e7c8c158c4b2b6ae0e75102b708ffb
Lesiv-Aleksandr <85366097+Lesiv-Aleksandr@users.noreply.github.com>
2021-10-26 22:05:48 +0300

    261021

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+
+const makegalleryItemsMarkur = galleryItems.map(galleryItem => {
+    const creatingNewModuleInHtml = 
+    `<a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image"  src="${galleryItem.preview}" alt="${galleryItem.description}" />
+  </a>`;
+    const addGalleryEl = galleryRef.insertAdjacentHTML('afterbegin',creatingNewModuleInHtml);
+   
+}).join("");
+galleryRef.addEventListener("click", onClick);
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250} );
+function onClick (e) { 
+   e.preventDefault();
+
+if (e.target.nodeName !== 'IMG') {
+  return;
+}
+}
+
